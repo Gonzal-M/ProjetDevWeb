8263333bd869f08f9eae90a0b9053b15354282d1
Marie <47717194+Clawchette@users.noreply.github.com>
2020-06-11 09:39:36 +0200

    del fichier

--- ProjetDevWeb/ProjetDevWeb/profil.php
+++
@@ -1,9 +0,0 @@
-<?php require_once("inc/header.inc.php"); ?>
-
-<!-- Page du profil -->
-<!-- affiche les infos et les biens mis en location d'un utilisateur -->
-<!-- Nécessite une connexion -->
-
-
-
-<?php require_once("inc/footer.inc.php"); ?>
