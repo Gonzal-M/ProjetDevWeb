9559cbe25cdec26addaf8124ede773c130e6be5c
Eddish <31674985+Rshzyyy@users.noreply.github.com>
2020-06-15 03:02:35 +0200

    index
    update index

--- ProjetDevWeb/index.php
+++ ProjetDevWeb/index.php
@@ -1,7 +1,35 @@
 <?php require_once("inc/header.inc.php"); ?>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
 
 <!-- Page d'accueil -->
 
 
+
+
+<section class="main">
+            <div class=main-content>
+    
+            </div>
+        </section>
+
+
+
+<!-- Categories -->
+    <div class="titre-categorie">
+            <h1>catégorie</h1>
+    </div>
+        <div class="flex-container">
+            <div class="categories">
+                <div class="categorie"> <img class="image" src="img/main/c1.jpg"> <a href="inscription.php"> <button>Cliquer-ici</button> </a>
+                    <p class="text_1">Inscrivez-vous</p>
+                </div>
+                <div class="categorie"> <img class="image" src="img/main/c2.jpg"> <a href="recherche.php"> <button>Cliquer-ici</button> </a>
+                    <p class="text_1">Rechercher une location</p>
+                </div>
+                <div class="categorie"> <img class="image" src="img/main/c3.jpg"> <a href="recherche.php"> <button>Cliquer-ici</button> </a>
+                    <p class="text_1">Contact</p>
+                </div>
+            </div>
 
 <?php require_once("inc/footer.inc.php"); ?>
